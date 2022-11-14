884c8d165dbd04c73a1b35bb365c709cbdf09e62
Fernando López Guevara <fernando.lguevara@gmail.com>
2022-11-14 16:32:51 -0300

    package lighter

---
+++ package.ts
@@ -0,0 +1,10 @@
+import p from "./dist/package.json";
+import { writeFileSync } from "fs";
+
+const data = JSON.stringify({
+    ...p,
+    devDependencies: undefined,
+    scripts: undefined
+}, null, '  ');
+
+writeFileSync('./dist/package.json', Buffer.from(data));
